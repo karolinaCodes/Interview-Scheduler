d0290a534e3059ee6c4574cb23bd008ba894296d
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-12-07 22:33:46 -0500

    fix useEffect bug and use getAppointmentsForDay function to format retrieved data

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,68 +3,16 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 import axios from "axios";
-
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       },
-//     },
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer: {
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       },
-//     },
-//   },
-//   {
-//     id: 5,
-//     time: "4pm",
-//   },
-// ];
+import {getAppointmentsForDay} from "../helpers/selectors";
 
 export default function Application(props) {
-  // Promise.all([
-  //   axios.get("http://localhost:8001/api/days"),
-  //   axios.get("http://localhost:8001/api/appointments"),
-  //   axios.get("http://localhost:8001/api/interviewers"),
-  // ]).then(all => {
-  //   console.log(all[0]); // first
-  //   console.log(all[1]); // second
-  //   console.log(all[2]); // third
-
-  //   const [first, second, third] = all;
-
-  //   console.log(first, second, third);
-  // });
-
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {},
+    appointments: [],
   });
 
-  const dailyAppointments = [];
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const setDay = day => setState({...state, day});
 
@@ -72,13 +20,11 @@ export default function Application(props) {
     Promise.all([
       axios.get("http://localhost:8001/api/days"),
       axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers"),
     ]).then(all => {
       setState(prev => ({
         ...prev,
-        day: all[0],
-        days: all[1],
-        appointments: all[2],
+        days: all[0].data,
+        appointments: all[1].data,
       }));
     });
   }, []);
