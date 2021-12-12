f639c13e9c4473ec2eb2d49f93bf8e3a4c31a772
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-12-12 14:20:01 -0500

    remove dead code

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -28,15 +28,12 @@ describe("Form", () => {
   });
 
   it("validates that the student name is not blank", () => {
-    /* 1. Create the mock onSave function */
     const onSave = jest.fn();
 
-    /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the name prop should be blank or undefined */
     const {getByText} = render(
       <Form interviewers={interviewers} onSave={onSave} />
     );
 
-    /* 3. Click the save button */
     fireEvent.click(getByText("Save"));
 
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
@@ -71,7 +68,7 @@ describe("Form", () => {
     const {getByText, getByPlaceholderText, queryByText} = render(
       <Form
         interviewers={interviewers}
-        name="Lydia Mill-Jones"
+        student="Lydia Mill-Jones"
         onSave={jest.fn()}
         onCancel={onCancel}
       />
