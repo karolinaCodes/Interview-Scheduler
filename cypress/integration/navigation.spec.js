48f7b3d89395c4698272e0368cdd361f6f6702d6
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-12-13 20:20:12 -0500

    add test tsecond test to cypress

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,4 +2,16 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+
+    cy.contains("li", "Tuesday").click();
+
+    cy.contains("li", "Tuesday").should(
+      "have.css",
+      "background-color",
+      "rgb(242, 242, 242)"
+    );
+  });
 });
