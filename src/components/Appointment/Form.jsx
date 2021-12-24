5b9399341e6a16cf18b06a7665055ef7c4947224
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-12-23 21:03:17 -0500

    add error handling for no inteviewer chosen

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -23,6 +23,11 @@ const Form = props => {
       return;
     }
 
+    if (interviewer === null) {
+      setError("You must choose an interviewer");
+      return;
+    }
+
     setError("");
     props.onSave(student, interviewer);
   }
