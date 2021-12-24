70510f13bf4127bc6ace6ea49be4be72743166e8
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-12-23 22:10:35 -0500

    remove .catch block for axios to handle error in Appointment component; removed hard-coded base URL

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -14,9 +14,9 @@ const useApplicationData = () => {
   // fetch days, appointments and interviewers and add to state
   useEffect(() => {
     Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers"),
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
     ])
       .then(all => {
         setState(prev => ({
@@ -47,7 +47,7 @@ const useApplicationData = () => {
 
     // return promise to update transition to show in appointment component
     return axios
-      .put(`http://localhost:8001/api/appointments/${id}`, {
+      .put(`/api/appointments/${id}`, {
         interview,
       })
       .then(() => {
@@ -56,8 +56,7 @@ const useApplicationData = () => {
           appointments,
           updatedDays,
         }));
-      })
-      .catch(err => console.error(err));
+      });
   }
 
   function cancelInterview(id) {
@@ -77,16 +76,13 @@ const useApplicationData = () => {
     updatedDays[dayIndex].spots = fetchFreeSpots(state, appointments);
 
     // return promise to update transition to show in appointment component
-    return axios
-      .delete(`http://localhost:8001/api/appointments/${id}`)
-      .then(() => {
-        setState(prev => ({
-          ...prev,
-          appointments,
-          updatedDays,
-        }));
-      })
-      .catch(err => console.error(err));
+    return axios.delete(`/api/appointments/${id}`).then(() => {
+      setState(prev => ({
+        ...prev,
+        appointments,
+        updatedDays,
+      }));
+    });
   }
 
   const setDay = day => setState(prev => ({...prev, day}));
